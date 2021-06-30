51ae2cb90e860abd980842f79bf7e3ada1836dd5
Maria Panayiotou <m.panayiotou@albourne.com>
2021-06-30 14:08:48 +0300

    rename class name RandomWord

--- algorithms/src/hello/RandomWord.java
+++ algorithms/src/hello/RandomWord.java
@@ -4,7 +4,7 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-public class RandomWorld {
+public class RandomWord {
 
 	public static void main(String[] args) {
 
